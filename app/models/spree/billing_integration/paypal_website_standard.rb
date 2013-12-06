6f9ae1df6c6e21fda426dafce3ef827105942c50
Rui Leitão <rmleitao@gmail.com>
2013-12-06 13:06:31 +0000

    Partially fixing attr_accessible for Rails 4

--- app/models/spree/billing_integration/paypal_website_standard.rb
+++ app/models/spree/billing_integration/paypal_website_standard.rb
@@ -1,6 +1,6 @@
 class Spree::BillingIntegration::PaypalWebsiteStandard < Spree::BillingIntegration
 
-  attr_accessible :preferred_account_email, :preferred_ipn_notify_host, :preferred_success_url, 
+  # attr_accessible :preferred_account_email, :preferred_ipn_notify_host, :preferred_success_url, 
     :preferred_paypal_url, :preferred_encryption, :preferred_certificate_id, 
     :preferred_currency, :preferred_language,
     :preferred_server, :preferred_test_mode
