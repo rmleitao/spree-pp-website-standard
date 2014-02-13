d5e419e260a4f37125abac2f40149795b470d2b3
Rui Leitão <rmleitao@gmail.com>
2014-02-13 18:14:45 +0000

    testing buying regular products. new gem version 1.1.10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Spree::Core::Engine.add_routes do
-#  post "paypal/confirm", :to => "paypal#confirm", :method => :post
-#  get "paypal/confirm", :to => "paypal#confirm", :method => :get
   resources :payment_notifications, :only => [:create]
 end
